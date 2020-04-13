6cd588f09e9d862e8f04da13127387f8f03f4d28
VirtualBrightPlayz <lego.trains.b@gmail.com>
2020-04-12 17:34:22 -0700

    added files

---
+++ NoRoleMute/MuteEventManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using EXILED;
+using EXILED.Extensions;
+
+namespace NoRoleMute
+{
+    public class MuteEventManager
+    {
+        public MutePlugin plugin;
+        public static List<string> notActualMutedPlayers = new List<string>();
+        bool isMuteTime = false;
+
+        public MuteEventManager(MutePlugin pl)
+        {
+            plugin = pl;
+            if (notActualMutedPlayers == null)
+                notActualMutedPlayers = new List<string>();
+        }
+
+        internal void RoundEnd()
+        {
+            isMuteTime = true;
+            MuteThem();
+        }
+
+        internal void RoundStart()
+        {
+            isMuteTime = false;
+            UnmuteThem();
+        }
+
+        public void UnmuteThem()
+        {
+            foreach (var ply in PlayerManager.players)
+            {
+                if (notActualMutedPlayers.Contains(ply.GetComponent<ReferenceHub>().GetUserId()))
+                {
+                    Log.Debug("Unmuting player " + ply.GetComponent<ReferenceHub>().GetNickname() + " as they have no role and were muted by this plugin.");
+                    ply.GetComponent<ReferenceHub>().Unmute();
+                    ply.GetComponent<ReferenceHub>().Broadcast(3, "[NoRoleMute] You have been unmuted.", false);
+                    notActualMutedPlayers.Remove(ply.GetComponent<ReferenceHub>().GetUserId());
+                }
+            }
+        }
+
+        internal void PlyJoin(PlayerJoinEvent ev)
+        {
+            if (isMuteTime)
+                MuteThem();
+            else
+                UnmuteThem();
+        }
+
+        internal void RACmd(ref RACommandEvent ev)
+        {
+            string[] args = ev.Command.Split(' ');
+            if (args[0].ToLower().StartsWith("mute"))
+            {
+                foreach (string str in args[1].Split('.'))
+                {
+                    ReferenceHub hub = Player.GetPlayer(int.Parse(str));
+                    notActualMutedPlayers.Remove(hub.GetUserId());
+                }
+            }
+        }
+
+        public void MuteThem()
+        {
+            foreach (var ply in PlayerManager.players)
+            {
+                if (ply.GetComponent<ReferenceHub>().IsMuted())
+                {
+                    continue;
+                }
+                if (!notActualMutedPlayers.Contains(ply.GetComponent<ReferenceHub>().GetUserId()) && string.IsNullOrWhiteSpace(ply.GetComponent<ReferenceHub>().serverRoles.GetUncoloredRoleString()))
+                {
+                    Log.Debug("Temp muting player " + ply.GetComponent<ReferenceHub>().GetNickname() + " as they have no role.");
+                    ply.GetComponent<ReferenceHub>().Mute();
+                    ply.GetComponent<ReferenceHub>().Broadcast(3, "[NoRoleMute] You have been muted.", false);
+                    notActualMutedPlayers.Add(ply.GetComponent<ReferenceHub>().GetUserId());
+                }
+            }
+        }
+
+        internal void RoundWait()
+        {
+            isMuteTime = true;
+            MuteThem();
+        }
+    }
+}
---
+++ NoRoleMute/MutePlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EXILED;
+
+namespace NoRoleMute
+{
+    public class MutePlugin : Plugin
+    {
+        internal MuteEventManager PLEV;
+        public override string getName => "NoRoleMute";
+
+        public override void OnDisable()
+        {
+            PLEV = null;
+        }
+
+        public override void OnEnable()
+        {
+            PLEV = new MuteEventManager(this);
+            Events.RoundEndEvent += PLEV.RoundEnd;
+            Events.RoundStartEvent += PLEV.RoundStart;
+            Events.WaitingForPlayersEvent += PLEV.RoundWait;
+            Events.RemoteAdminCommandEvent += PLEV.RACmd;
+            Events.PlayerJoinEvent += PLEV.PlyJoin;
+        }
+
+        public override void OnReload()
+        {
+        }
+    }
+}
